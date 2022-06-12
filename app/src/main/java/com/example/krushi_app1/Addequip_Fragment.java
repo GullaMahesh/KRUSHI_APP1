a0303024b5a0e5c7d5406cd08fc95e4e247cfa06
GullaMahesh <maheshgulla5599@gmail.com>
2022-06-12 16:07:22 +0530

    upto fragments

--- app/src/main/java/com/example/krushi_app1/Addequip_Fragment.java
+++ app/src/main/java/com/example/krushi_app1/Addequip_Fragment.java
@@ -20,52 +20,16 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Addequip_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Addequip_Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Addequip_Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Addequip_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Addequip_Fragment newInstance(String param1, String param2) {
-        Addequip_Fragment fragment = new Addequip_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 EditText t1;
     EditText t2;
     EditText t3;
@@ -84,7 +48,7 @@ EditText t1;
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View view= inflater.inflate(R.layout.fragment_addequip_, container, false);
+        View view = inflater.inflate(R.layout.fragment_addequip_, container, false);
         img=view.findViewById(R.id.img);
         b=view.findViewById(R.id.submit);
         b2=view.findViewById(R.id.browse);
@@ -96,31 +60,32 @@ EditText t1;
         t6=view.findViewById(R.id.t6);
         t7=view.findViewById(R.id.t7);
         t8=view.findViewById(R.id.t8);
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Dexter.withContext(getContext())
-                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                        .withListener(new PermissionListener() {
-                            @Override
-                            public void onPermissionGranted(PermissionGrantedResponse response)
-                            {
-                                Intent intent=new Intent(Intent.ACTION_PICK);
-                                intent.setType("image/*");
-                                startActivityForResult(Intent.createChooser(intent,"Select Image File"),1);
-                            }
-                            @Override
-                            public void onPermissionDenied(PermissionDeniedResponse response) {
+       b2.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Dexter.withContext(getActivity())
+                       .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                       .withListener(new PermissionListener() {
+                           @Override
+                           public void onPermissionGranted(PermissionGrantedResponse response) {
+                               Intent intent = new Intent(Intent.ACTION_PICK);
+                               intent.setType("image/*");
 
-                            }
+                           }
 
-                            @Override
-                            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
-                                token.continuePermissionRequest();
-                            }
-                        }).check();
-            }
-        });
+                           @Override
+                           public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                           }
+
+                           @Override
+                           public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                               token.continuePermissionRequest();
+                           }
+                       }).check();
+
+           }
+       });
 
         return view;
     }
